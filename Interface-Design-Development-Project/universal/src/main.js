e4ac0da71900a47dd58b2ade2434349b088ae80e
Shiva Gupta <gshiva53@gmail.com>
2022-05-04 19:05:18 +1000

    [ADD]; adding @popperjs/core

--- Interface-Design-Development-Project/universal/src/main.js
+++ Interface-Design-Development-Project/universal/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// importing bootstrap
+//importing bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
-// importing jquery 
+//importing jquery 
 import 'jquery'
+//importing popper js 
+import '@popperjs/core'
 
 createApp(App).mount('#app')
