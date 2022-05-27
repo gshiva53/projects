455295ea7a3781e7403faebdae425c777fa26677
Shiva Gupta <gshiva53@gmail.com>
2022-05-27 16:32:58 +1000

    [MODIFY] : CRUD operations can now be performed on Bugs db

--- Bug-Tracker/BT-JPA/src/Bugs/BugsDbTestApp.java
+++ Bug-Tracker/BT-JPA/src/Bugs/BugsDbTestApp.java
@@ -24,13 +24,22 @@ public class BugsDbTestApp {
     public static void main(String[] args) {
         BugsDbTestApp client = new BugsDbTestApp();
 
-        BugsDTO bugsDTO = new BugsDTO(00001, "npm fix", "npm package needs fixing", "fixing", "shiva", "high");
-        boolean result = client.createRecord(bugsDTO); 
-        client.showCreateResult(result, bugsDTO);
-    }
-
-    public boolean createRecord(BugsDTO bugsDTO) {
-        return bugsDb.createRecord(bugsDTO);
+//        //TEST: creating a record in the bug db
+//        BugsDTO bugsDTO = new BugsDTO(00001, "npm fix", "npm package needs fixing", "fixing", "shiva", "high");
+//        boolean result = client.createRecord(bugsDTO);
+//        client.showCreateResult(result, bugsDTO);
+//
+//        //TEST: reading a record from the bug db
+//        client.showGetResult(client.getRecord(00001));
+//
+//        //TEST: updating the record from the bug db
+//        BugsDTO updatebugsDTO = new BugsDTO(00001, "npm fix", "npm package needs fixing", "fixing", "shiva", "medium");
+//        boolean updateresult = client.updateRecord(updatebugsDTO);
+//        client.showUpdateResult(updateresult, updatebugsDTO);
+        
+        //TEST: deleting the record from the bug db 
+        boolean deleteresult = client.deleteRecord(3);
+        client.showDeleteResult(deleteresult, 3); 
     }
 
     public void showCreateResult(boolean result, BugsDTO bugsDTO) {
@@ -41,5 +50,56 @@ public class BugsDbTestApp {
             System.out.println("Record with primary key " + bugsDTO.getBugid()
                     + " could not be created in the database table!");
         }
+    }
+
+    //printing information from bugDTO
+    public void showGetResult(BugsDTO bugDTO) {
+        System.out.println("ID; " + bugDTO.getBugid()
+                + "\nName: " + bugDTO.getBugname()
+                + "\nDescription: " + bugDTO.getBugdesc()
+                + "\nStatus: " + bugDTO.getBugstatus()
+                + "\nCreated By: " + bugDTO.getBugcreatedby()
+                + "\nPriority: " + bugDTO.getBugpriority());
+    }
+
+    //print the information after the exceution of updating the record
+    public void showUpdateResult(boolean result, BugsDTO bugDTO) {
+        if (result) {
+            System.out.println("Record with primary key " + bugDTO.getBugid()
+                    + " has been updated in the database table.");
+        } else {
+            System.out.println("Record with primary key " + bugDTO.getBugid()
+                    + " has NOT been updated in the database table.");
+        }
+    }
+
+    //print the information after the deleting of the record
+    public void showDeleteResult(boolean result, int bugid) {
+        if (result) {
+            System.out.println("Record with primary key " + bugid
+                    + " has been deleted in the database table.");
+        } else {
+            System.out.println("Record with primary key " + bugid
+                    + " could not be deleted in the database table!");
+        }
+    }
+
+    //wrapper method to expose functionality of the createRecord() function in bugsDb
+    public boolean createRecord(BugsDTO bugsDTO) {
+        return bugsDb.createRecord(bugsDTO);
+    }
+
+    //wrapper method to expose functionality of the getRecord() function in bugsDb
+    public BugsDTO getRecord(int bugid) {
+        return bugsDb.getRecord(bugid);
+    }
+
+    //wrapper method to expose functionality of the updateRecord() function in bugsDb
+    public boolean updateRecord(BugsDTO bugsDTO) {
+        return bugsDb.updateRecord(bugsDTO);
+    }
+
+    public boolean deleteRecord(int bugid) {
+        return bugsDb.deleteRecord(bugid);
     }
 }
--- Bug-Tracker/BT-JPA/src/bt/jpa/MyBugsDb.java
+++ Bug-Tracker/BT-JPA/src/bt/jpa/MyBugsDb.java
@@ -1,6 +1,5 @@
 package bt.jpa;
 
-
 import Bugs.BugsDTO;
 import entity.BugsDb;
 import javax.persistence.EntityManager;
@@ -9,67 +8,141 @@ import javax.persistence.Persistence;
 
 /**
  *
- * @author Shiva Gupta
- * MyBugsDb is a JPA controller class. Refer to week 3 lab. 
+ * @author Shiva Gupta MyBugsDb is a JPA controller class. Refer to week 3 lab.
  */
 public class MyBugsDb {
-    private EntityManager em = null; 
-    
+
+    private EntityManager em = null;
+
     public MyBugsDb() {
         //using default persisence unit defined in the persistence.xml file 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("BT-EntityPU"); 
-        em = emf.createEntityManager(); 
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("BT-EntityPU");
+        em = emf.createEntityManager();
     }
-    
+
     public EntityManager getEntityManager() {
-        return em; 
+        return em;
     }
-    
-    public BugsDb findBugsDb (int bugsId) {
-        return em.find(BugsDb.class, bugsId); 
+
+    public BugsDb findBugsDb(int bugsId) {
+        return em.find(BugsDb.class, bugsId);
     }
-    
-    public boolean createBugsInDb(BugsDb bug) throws Exception {
+
+    public boolean createBugInDb(BugsDb bug) throws Exception {
         try {
             if (findBugsDb(bug.getBugid()) != null) {
                 //bug with same bugid already exists
                 //false -> bug was not created
-                return false; 
+                return false;
             }
             //bug does not exist in the database 
             em.getTransaction().begin();
             em.persist(bug);
-            em.getTransaction().commit(); 
-            
+            em.getTransaction().commit();
+
             //true -> bug was created in the DB
-            return true; 
+            return true;
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    private boolean updateBugInDb(BugsDb bug) throws Exception {
+        try {
+            if (findBugsDb(bug.getBugid()) == null) {
+                return false;
+            }
+            em.getTransaction().begin();
+            em.merge(bug);
+            em.getTransaction().commit();
+
+            return true;
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    private boolean deleteBugInDb(int bugid) throws Exception {
+        try {
+            if (findBugsDb(bugid) == null) {
+                //no bugs are found in the database
+                //false -> delete can not be performed
+                return false;
+            }
+
+            em.getTransaction().begin();
+            em.remove(findBugsDb(bugid));
+            em.getTransaction().commit();
         } catch (Exception ex) {
             throw ex; 
         }
+        return true; 
     }
-    
-    public boolean createRecord(BugsDTO bugsDTO) {
-        BugsDb bug = this.DTO2DAO(bugsDTO); 
-        
-        boolean result = false; 
+
+    //get the bug information as a DTO object using bug id
+    public BugsDTO getRecord(int bugid) {
         try {
-            result = this.createBugsInDb(bug); 
+            BugsDb bug = findBugsDb(bugid);
+            return this.DAO2DTO(bug);
         } catch (Exception ex) {
         }
+        return null;
+    }
+
+    public boolean createRecord(BugsDTO bugsDTO) {
+        BugsDb bug = this.DTO2DAO(bugsDTO);
+
+        boolean result = false;
+        try {
+            result = this.createBugInDb(bug);
+        } catch (Exception ex) {
+        }
+
+        return result;
+    }
+    
+    public boolean updateRecord(BugsDTO bugsDTO) {
+        BugsDb bug = this.DTO2DAO(bugsDTO); 
+        boolean result = false; 
         
+        try {
+            result = this.updateBugInDb(bug); 
+        } catch (Exception ex) {
+        }
         return result; 
     }
     
+    public boolean deleteRecord(int bugid) {
+        boolean result = false; 
+        
+        try {
+            result = this.deleteBugInDb(bugid); 
+        } catch (Exception ex) {
+        }
+        return result; 
+    }
+
     private BugsDb DTO2DAO(BugsDTO bugsDTO) {
         BugsDb bug = new BugsDb();
-        
-        bug.setBugid(bugsDTO.getBugid()); 
+
+        bug.setBugid(bugsDTO.getBugid());
         bug.setBugname(bugsDTO.getBugname());
         bug.setBugdesc(bugsDTO.getBugdesc());
         bug.setBugstatus(bugsDTO.getBugstatus());
         bug.setBugcreatedby(bugsDTO.getBugcreatedby());
         bug.setBugpriority(bugsDTO.getBugpriority());
-        
-        return bug; 
+
+        return bug;
+    }
+
+    private BugsDTO DAO2DTO(BugsDb bug) {
+        BugsDTO bugDTO = new BugsDTO(bug.getBugid(),
+                bug.getBugname(),
+                bug.getBugdesc(),
+                bug.getBugstatus(),
+                bug.getBugcreatedby(),
+                bug.getBugpriority());
+
+        return bugDTO;
     }
 }
