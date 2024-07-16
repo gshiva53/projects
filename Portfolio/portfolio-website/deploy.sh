b430134be33d74b1d06587fe8df06f31d117e070
Shiva Gupta <gshiva53@gmail.com>
2024-07-16 20:30:15 +0930

    [MODIFY]: deploying nginx file to the server

--- Portfolio/portfolio-website/deploy.sh
+++ Portfolio/portfolio-website/deploy.sh
@@ -4,7 +4,10 @@ git checkout main
 echo "building app..." 
 npm run build 
 
-echo "Deploying files to Server" 
+echo "Deploying build files to Server" 
 scp -r dist/portfolio-website/* shiva@139.144.99.189:/var/www/139.144.99.189/
 
-echo "Done!" 
+echo "Deploying nginx file to Server" 
+scp -r 139.144.99.189 shiva@139.144.99.189:/etc/nginx/sites-enabled/139.144.99.189
+
+echo "Done!"
